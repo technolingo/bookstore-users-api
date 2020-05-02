1fbf02ca01fb3ac69908af496aeba80d06e97e97
Zilong Li <zilong.li@deliveryhero.com>
2020-05-02 16:42:56 +0200

    controller create user

--- controllers/users.go
+++ controllers/users.go
@@ -1,17 +1,55 @@
 package controllers
 
-func CreateUser() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+
+	"github.com/technolingo/bookstore-users-api/models"
+	"github.com/technolingo/bookstore-users-api/services"
+)
+
+// CreateUser creates a user object according to the payload
+func CreateUser(c *gin.Context) {
+	var user models.User
+
+	// bytes, err := ioutil.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	return
+	// }
+	// if err := json.Unmarshal(bytes, &user); err != nil {
+	// 	return
+	// }
+	if err := c.ShouldBindJSON(&user); err != nil {
+		// TODO: handle error
+		return
+	}
+
+	result, err := services.CreateUser(&user)
+	if err != nil {
+		// TODO: handle error
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
-func RetrieveUser() {
-
+// RetrieveUser gets a user object according to the `user_id`
+func RetrieveUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
 }
 
-func UpdateUser() {
-
+// SearchUser returns a list of users matching the query param supplied
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
 }
 
-func DeleteUser() {
+// UpdateUser updates an existing user according to the payload
+func UpdateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
+}
 
+// DeleteUser removes the user object according to the `user_id`
+func DeleteUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
 }
--- services/users.go
+++ services/users.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/technolingo/bookstore-users-api/models"
+)
+
+// CreateUser processes the buisness logic and creates the user
+func CreateUser(u *models.User) (*models.User, error) {
+	return &models.User{}, nil
+}
